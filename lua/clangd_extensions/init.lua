975ccb9d67011f3fae34bd7366da09252bc37a3f
Chinmay Dalal <dalal.chinmay.0101@gmail.com>
2022-02-08 23:43:19 +0530

    Move commands to on_attach

--- lua/clangd_extensions/init.lua
+++ lua/clangd_extensions/init.lua
@@ -4,17 +4,35 @@ local M = {}
 
 M.hint_aucmd_set_up = false
 
+local commands = [[
+if !exists(':ClangdAST')
+
+  function s:memuse_compl(_a,_b,_c)
+      return ['expand_preamble']
+  endfunction
+
+  command ClangdSetInlayHints lua require('clangd_extensions.inlay_hints').set_inlay_hints()
+  command ClangdDisableInlayHints lua require('clangd_extensions.inlay_hints').disable_inlay_hints()
+  command ClangdToggleInlayHints lua require('clangd_extensions.inlay_hints').toggle_inlay_hints()
+  command -range ClangdAST lua require('clangd_extensions.ast').display_ast(<line1>, <line2>)
+  command ClangdTypeHeirarchy lua require('clangd_extensions.type_heirarchy').show_heirarchy()
+  command ClangdSymbolInfo lua require('clangd_extensions.symbol_info').show_symbol_info()
+  command -nargs=? -complete=customlist,s:memuse_compl ClangdMemoryUsage lua require('clangd_extensions.memory_usage').show_memory_usage('<args>' == 'expand_preamble')
+
+endif
+]]
+
 function M.setup(opts)
     -- Set up extensions, get lspconfig opts
     config.setup(opts)
     -- Set up autocommands for inlay hints
-    if config.options.extensions.autoSetHints and not M.hint_aucmd_set_up then
+    if config.options.extensions.autoSetHints then
         local old_func = config.options.server.on_attach
         config.options.server.on_attach = function(client, bufnr)
             old_func(client, bufnr)
             require("clangd_extensions.inlay_hints").setup_autocmd()
             require("clangd_extensions.inlay_hints").set_inlay_hints()
-            M.hint_aucmd_set_up = true
+            vim.cmd(commands)
         end
     end
     -- Call lspconfig setup
