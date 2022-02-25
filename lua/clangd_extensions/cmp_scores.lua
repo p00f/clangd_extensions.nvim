8495c4e8f0a8b6a7b5e44cb117a8db199d46251b
Chinmay Dalal <dalal.chinmay.0101@gmail.com>
2022-02-25 14:50:47 +0530

    check if both entries' `completion_item`s have `score`s

--- lua/clangd_extensions/cmp_scores.lua
+++ lua/clangd_extensions/cmp_scores.lua
@@ -1,6 +1,6 @@
 return function(entry1, entry2)
     local diff
-    if entry1.completion_item.score then
+    if entry1.completion_item.score and entry2.completion_item.score then
         diff = (entry2.completion_item.score * entry2.score)
             - (entry1.completion_item.score * entry1.score)
     else
