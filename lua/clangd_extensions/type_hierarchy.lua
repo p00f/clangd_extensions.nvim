7f003d48d2b846aeb364d153d7defc7e3cea42bd
Chinmay Dalal <dalal.chinmay.0101@gmail.com>
2023-07-23 22:05:26 +0530

    type heirarchy: reuse source code window for jumping to type definition

--- lua/clangd_extensions/type_hierarchy.lua
+++ lua/clangd_extensions/type_hierarchy.lua
@@ -40,23 +40,38 @@ local function handler(err, TypeHierarchyItem, ctx)
     if err or not TypeHierarchyItem then
         return
     else
+        local oldwin = api.nvim_get_current_win()
         vim.cmd(fmt([[split %s:\ type\ hierarchy]], TypeHierarchyItem.name))
         local bufnr = vim.api.nvim_get_current_buf()
         -- retrieve offset_encoding for later use
         M.offset_encoding = vim.lsp.get_client_by_id(ctx.client_id).offset_encoding
         type_pos_mappings[bufnr] = {}
         local lines = format_tree(TypeHierarchyItem, {}, {}, "", type_pos_mappings[bufnr])
-        vim.bo.modifiable = true
         api.nvim_buf_set_lines(bufnr, 0, -1, true, lines)
         vim.bo.modifiable = false
         vim.bo.filetype = "ClangdTypeHierarchy"
         vim.bo.buftype = "nofile"
-        vim.bo.bufhidden = "hide"
+        vim.bo.bufhidden = "wipe"
         vim.bo.buflisted = true
         api.nvim_win_set_option(0, "number", false)
         api.nvim_win_set_option(0, "relativenumber", false)
         api.nvim_win_set_option(0, "spell", false)
         api.nvim_win_set_option(0, "cursorline", false)
+
+        vim.cmd([[
+        syntax clear
+        syntax match ClangdTypeName "\( \{2,\}• \)\@<=\w\+\(:\)\@="
+        ]])
+        vim.api.nvim_set_hl(0, "ClangdTypeName", { link = "Underlined" })
+        vim.keymap.set("n", "gd", function()
+            local word = vim.fn.expand("<cWORD>")
+            word = string.gsub(word, ":$", "")
+            local location = type_pos_mappings[bufnr][word]
+            if location ~= nil then
+                api.nvim_set_current_win(oldwin)
+                vim.lsp.util.jump_to_location(location, M.offset_encoding)
+            end
+        end, { buffer = bufnr, desc = "go to definition of cursor" })
     end
 end
 
@@ -74,63 +89,5 @@ function M.show_hierarchy()
         direction = 2,
     }, handler)
 end
-
-local type_hierarchy_group = vim.api.nvim_create_augroup("ClangdTypeHierarchy", {
-    clear = true
-})
-
-local function setup_type_hierarchy_buffer(bufnr)
-    vim.cmd([[
-    syntax clear
-    syntax match ClangdTypeName "\( \{2,\}• \)\@<=\w\+\(:\)\@="
-    ]])
-    vim.api.nvim_set_hl(0, "ClangdTypeName", {link = "Underlined"})
-    vim.keymap.set("n", "gd", function ()
-        local word = vim.fn.expand("<cWORD>")
-        word = string.gsub(word, ":$", "")
-        local location = type_pos_mappings[bufnr][word]
-        if location ~= nil then
-            vim.lsp.util.jump_to_location(location, M.offset_encoding)
-        end
-    end, { buffer = bufnr, desc = "go to definition of cursor" })
-end
-
--- setup when buffer is first created
-vim.api.nvim_create_autocmd("FileType", {
-    group = type_hierarchy_group,
-    pattern = "ClangdTypeHierarchy",
-    callback = function ()
-        local bufnr = vim.api.nvim_get_current_buf()
-        setup_type_hierarchy_buffer(bufnr)
-    end
-})
-
--- setup when the hidden/deleted buffer is loaded
-vim.api.nvim_create_autocmd({ "BufReadPost" }, {
-    group = type_hierarchy_group,
-    pattern = "*",
-    callback = function ()
-        local bufnr = vim.api.nvim_get_current_buf()
-        -- below doesn't work when buffer is deleted and then opened
-        -- if vim.bo[bufnr].filetype ~= "ClangdTypeHierarchy" then
-        --     return
-        -- end
-        local bufname = vim.api.nvim_buf_get_name(bufnr)
-        if not vim.endswith(bufname, ": type hierarchy") then
-            return
-        end
-        setup_type_hierarchy_buffer(bufnr)
-    end
-})
-
-vim.api.nvim_create_autocmd("BufWipeout", {
-    group = type_hierarchy_group,
-    pattern = "ClangdTypeHierarchy",
-    callback = function ()
-       local bufnr = vim.api.nvim_get_current_buf()
-       type_pos_mappings[bufnr] = nil
-    end
-})
-
 
 return M
