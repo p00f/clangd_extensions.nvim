69dba0e80e9d50733328b90a1a313be60c681e2a
Chinmay Dalal <dalal.chinmay.0101@gmail.com>
2023-07-25 11:55:58 +0530

    call `require()` inside keymap instead of eagerly

--- lua/clangd_extensions/init.lua
+++ lua/clangd_extensions/init.lua
@@ -36,27 +36,33 @@ end
 function M.setup_comands()
     -- nvim_get_commands would work here, but we don't need _all_ the commands, just an existence check
     if vim.fn.exists(':ClangdAST') == 0 then
-        local inlay_hints = require('clangd_extensions.inlay_hints')
-        local ast = require('clangd_extensions.ast')
-        local type_hierarchy = require('clangd_extensions.type_hierarchy')
-        local symbol_info = require('clangd_extensions.symbol_info')
-        local memory_usage = require('clangd_extensions.memory_usage')
-
-        vim.api.nvim_create_user_command('ClangdSetInlayHints', inlay_hints.set_inlay_hints, { nargs = 0 })
-        vim.api.nvim_create_user_command('ClangdDisableInlayHints', inlay_hints.disable_inlay_hints, { nargs = 0 })
-        vim.api.nvim_create_user_command('ClangdToggleInlayHints', inlay_hints.toggle_inlay_hints, { nargs = 0 })
-        vim.api.nvim_create_user_command('ClangAST', function(opts) ast.display_ast(opts.line1, opts.line2) end, { range = true, nargs = 0 })
-        vim.api.nvim_create_user_command('ClangdTypeHierarchy', type_hierarchy.show_hierarchy, { nargs = 0 })
-        vim.api.nvim_create_user_command('ClangdSymbolInfo', symbol_info.show_symbol_info, { nargs = 0 })
+        vim.api.nvim_create_user_command('ClangdSetInlayHints', function()
+            require("clangd_extensions.inlay_hints").set_inlay_hints()
+        end, { nargs = 0 })
+        vim.api.nvim_create_user_command('ClangdDisableInlayHints', function()
+            require("clangd_extensions.inlay_hints").disable_inlay_hints()
+        end, { nargs = 0 })
+        vim.api.nvim_create_user_command('ClangdToggleInlayHints', function()
+            require("clangd_extensions.inlay_hints").toggle_inlay_hints()
+        end, { nargs = 0 })
+        vim.api.nvim_create_user_command('ClangAST', function(opts)
+            require("clangd_extensions.ast").display_ast(opts.line1, opts.line2)
+        end, { range = true, nargs = 0 })
+        vim.api.nvim_create_user_command('ClangdTypeHierarchy', function()
+            require("clangd_extensions.type_hierarchy").show_hierarchy()
+        end, { nargs = 0 })
+        vim.api.nvim_create_user_command('ClangdSymbolInfo', function()
+            require("clangd_extensions.symbol_info").show_symbol_info()
+        end, { nargs = 0 })
         vim.api.nvim_create_user_command(
             'ClangdMemoryUsage',
             function(opts)
-                memory_usage.show_memory_usage(opts.args == 'expand_preamble')
+                require("clangd_extensions.memory_usage").show_memory_usage(opts.args == 'expand_preamble')
             end,
             {
                 nargs = '?',
-                complete = function(_,_,_)
-                    return {'expand_preamble'}
+                complete = function(_, _, _)
+                    return { 'expand_preamble' }
                 end
             }
         )
