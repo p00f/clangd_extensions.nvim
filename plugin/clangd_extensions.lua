18ef418986024f76a3dc23ea562eb9adca2db5f7
Chinmay Dalal <dalal.chinmay.0101@gmail.com>
2023-08-08 11:01:02 +0530

    remove redundant nargs = 0

--- plugin/clangd_extensions.lua
+++ plugin/clangd_extensions.lua
@@ -1,22 +1,30 @@
-vim.api.nvim_create_user_command('ClangdSetInlayHints', function()
+local create_command = vim.api.nvim_create_user_command
+
+create_command('ClangdSetInlayHints', function()
     require("clangd_extensions.inlay_hints").set_inlay_hints()
-end, { nargs = 0 })
-vim.api.nvim_create_user_command('ClangdDisableInlayHints', function()
+end)
+
+create_command('ClangdDisableInlayHints', function()
     require("clangd_extensions.inlay_hints").disable_inlay_hints()
-end, { nargs = 0 })
-vim.api.nvim_create_user_command('ClangdToggleInlayHints', function()
+end)
+
+create_command('ClangdToggleInlayHints', function()
     require("clangd_extensions.inlay_hints").toggle_inlay_hints()
-end, { nargs = 0 })
-vim.api.nvim_create_user_command('ClangdAST', function(opts)
+end)
+
+create_command('ClangdAST', function(opts)
     require("clangd_extensions.ast").display_ast(opts.line1, opts.line2)
-end, { range = true, nargs = 0 })
-vim.api.nvim_create_user_command('ClangdTypeHierarchy', function()
+end, { range = true })
+
+create_command('ClangdTypeHierarchy', function()
     require("clangd_extensions.type_hierarchy").show_hierarchy()
-end, { nargs = 0 })
-vim.api.nvim_create_user_command('ClangdSymbolInfo', function()
+end)
+
+create_command('ClangdSymbolInfo', function()
     require("clangd_extensions.symbol_info").show_symbol_info()
-end, { nargs = 0 })
-vim.api.nvim_create_user_command(
+end)
+
+create_command(
     'ClangdMemoryUsage',
     function(opts)
         require("clangd_extensions.memory_usage").show_memory_usage(opts.args == 'expand_preamble')
