0036bb88ed9bf1e7c7dc80a92b425cba1091328b
Chinmay Dalal <dalal.chinmay.0101@gmail.com>
2023-08-08 11:02:32 +0530

    fix command decl

--- plugin/clangd_extensions.lua
+++ plugin/clangd_extensions.lua
@@ -2,15 +2,15 @@ local create_command = vim.api.nvim_create_user_command
 
 create_command('ClangdSetInlayHints', function()
     require("clangd_extensions.inlay_hints").set_inlay_hints()
-end)
+end, {})
 
 create_command('ClangdDisableInlayHints', function()
     require("clangd_extensions.inlay_hints").disable_inlay_hints()
-end)
+end, {})
 
 create_command('ClangdToggleInlayHints', function()
     require("clangd_extensions.inlay_hints").toggle_inlay_hints()
-end)
+end, {})
 
 create_command('ClangdAST', function(opts)
     require("clangd_extensions.ast").display_ast(opts.line1, opts.line2)
@@ -18,11 +18,11 @@ end, { range = true })
 
 create_command('ClangdTypeHierarchy', function()
     require("clangd_extensions.type_hierarchy").show_hierarchy()
-end)
+end, {})
 
 create_command('ClangdSymbolInfo', function()
     require("clangd_extensions.symbol_info").show_symbol_info()
-end)
+end, {})
 
 create_command(
     'ClangdMemoryUsage',
