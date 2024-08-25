6fbc0bcf4e85ffcd4f9b0da67b454b0a73735946
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-08-24 19:39:29 -0600

    Revert "refactor(source_header): Rewrote module return structure for extensions."
    This reverts commit 69a7f29ae217590850c30167b69a36e3e4e1f257.

--- lua/clangd_extensions/switch_source_header.lua
+++ lua/clangd_extensions/switch_source_header.lua
@@ -14,12 +14,10 @@ local function handler(_err, uri)
     }, {})
 end
 
-local M = {}
-
-function M.switch_source_header()
-    vim.lsp.buf_request(0, "textDocument/switchSourceHeader", {
-        uri = vim.uri_from_bufnr(0),
-    }, handler)
-end
-
-return M
+return {
+    switch_source_header = function()
+        vim.lsp.buf_request(0, "textDocument/switchSourceHeader", {
+            uri = vim.uri_from_bufnr(0),
+        }, handler)
+    end,
+}
