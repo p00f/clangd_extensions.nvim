f28f6f7d0ad64dda9055f9832f1bc5c7e8dd442e
Chinmay Dalal <~chinmay/public-inbox@lists.sr.ht>
2026-03-29 12:19:09 -0400

    use nix for CI

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              emmylua-check
+            ];
+          };
+        });
+    };
+}
